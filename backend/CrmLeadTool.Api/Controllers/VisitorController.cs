afe3d497d2760f4de126e2f7c1dd41834e49c856
Jenish Umaraniya <jenishumaraniya@gmail.com>
2026-08-18 16:14:13 +0530

    Updated Backend Api conflict resolve

--- backend/CrmLeadTool.Api/Controllers/VisitorController.cs
+++ backend/CrmLeadTool.Api/Controllers/VisitorController.cs
@@ -1,65 +1,28 @@
-using CrmLeadTool.Api.DTOs; 
+using CrmLeadTool.Api.DTOs;
+using CrmLeadTool.Api.Services;
+using Microsoft.AspNetCore.Mvc;
 
-using CrmLeadTool.Api.Services; 
+namespace CrmLeadTool.Api.Controllers;
 
-using Microsoft.AspNetCore.Mvc; 
+[ApiController]
+[Route("api/visitor")]
+public class VisitorController : ControllerBase
+{
+    private readonly VisitorService _visitorService;
 
- 
+    public VisitorController(VisitorService visitorService)
+    {
+        _visitorService = visitorService;
+    }
 
-namespace CrmLeadTool.Api.Controllers; 
-
- 
-
-[ApiController] 
-
-[Route("api/visitor")] 
-
-public class VisitorController : ControllerBase 
-
-{ 
-
-    private readonly VisitorService _visitorService; 
-
- 
-
-    public VisitorController( 
-
-        VisitorService visitorService) 
-
-    { 
-
-        _visitorService = visitorService; 
-
-    } 
-
- 
-
-    [HttpPost("create")] 
-
-    public async Task<IActionResult> Create( 
-
-        CreateVisitorDto dto) 
-
-    { 
-
-        var visitor = 
-
-            await _visitorService.CreateVisitor( 
-
-                dto.ConsentStatus ?? "UNKNOWN"); 
-
- 
-
-        return Ok(new 
-
-        { 
-
-            visitorId = visitor.VisitorId, 
-
-            anonymousId = visitor.AnonymousId 
-
-        }); 
-
-    } 
-
+    [HttpPost("create")]
+    public async Task<IActionResult> Create(CreateVisitorDto dto)
+    {
+        var visitor = await _visitorService.CreateVisitor(dto.ConsentStatus ?? "UNKNOWN");
+        return Ok(new
+        {
+            visitorId = visitor.VisitorId,
+            anonymousId = visitor.AnonymousId
+        });
+    }
 }
--- backend/CrmLeadTool.Api/obj/Debug/net10.0/CrmLeadTool.Api.AssemblyInfo.cs
+++ backend/CrmLeadTool.Api/obj/Debug/net10.0/CrmLeadTool.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrmLeadTool.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c885427e1fecedacd18a0425d8fff4820b3880a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdc808e898b95b055e6e9647b087ce8324714816")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrmLeadTool.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrmLeadTool.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
